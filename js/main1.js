56680fddfc25a87d9d1df6c9579aa42385bf7e6f
Burrypony <kiril.chernysh@gmail.com>
2020-02-02 12:57:34 +0200

    first commit

---
+++ js/main1.js
@@ -0,0 +1,21 @@
+(function($) {
+    "use strict"; // Start of use strict
+
+    // Initialize and Configure Scroll Reveal Animation
+    window.sr = ScrollReveal();
+    sr.reveal('.dog', {
+        duration: 600,
+        scale: 0.3,
+        distance: '0px'
+    }, 200);
+    sr.reveal('.sr-button', {
+        duration: 1000,
+        delay: 200
+    });
+    sr.reveal('.sr-contact', {
+        duration: 600,
+        scale: 0.3,
+        distance: '0px'
+    }, 300);
+
+})(jQuery); // End of use strict
